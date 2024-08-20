47a8264b3b0b180a72aaf65b026890215dc3da18
DocSova <gnikita95@gmail.com>
2024-08-20 13:08:12 +0300

    fix migrations

--- backend/recipes/migrations/0001_initial.py
+++ backend/recipes/migrations/0001_initial.py
@@ -28,7 +28,7 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('username', models.CharField(max_length=150, unique=True, validators=[recipes.validators.username_validator], verbose_name='username')),
+                ('username', models.CharField(max_length=150, unique=True, validators=[recipes.validators.validate_username], verbose_name='username')),
                 ('first_name', models.CharField(max_length=150, verbose_name='Имя')),
                 ('last_name', models.CharField(max_length=150, verbose_name='Фамилия')),
                 ('email', models.EmailField(max_length=254, unique=True, verbose_name='email')),
@@ -88,7 +88,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200, unique=True, verbose_name='Название')),
-                ('color', models.CharField(max_length=7, unique=True, validators=[recipes.validators.color_validator], verbose_name='Цвет')),
+                ('color', models.CharField(max_length=7, unique=True, validators=[recipes.validators.validate_color], verbose_name='Цвет')),
                 ('slug', models.SlugField(max_length=200, unique=True, verbose_name='Слаг')),
             ],
             options={
