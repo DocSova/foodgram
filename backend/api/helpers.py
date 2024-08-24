e40976c32ea8bec739dad6925cad007edac1678e
DocSova <gnikita95@gmail.com>
2024-08-24 03:28:25 +0300

    test

--- backend/api/helpers.py
+++ backend/api/helpers.py
@@ -6,10 +6,7 @@ from recipes.models import ShortLink
 def redirect_link(request, short_link):
     """Метод переадресации ссылок."""
 
-    print(short_link)
-
-    link = get_object_or_404(
-        ShortLink,
+    link = ShortLink.objects.get(
         short_link=short_link
     )
     return redirect(link.full_link)
